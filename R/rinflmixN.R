5b78ffecf927767be22649d2752d41478d475ae9
Ryan T. Godwin <ryan.godwin@umanitoba.ca>
2018-08-13 15:56:56 -0500

    Fixed rinflmixN

--- R/rinflmixN.R
+++ R/rinflmixN.R
@@ -13,7 +13,7 @@
 #' @import utils
 #' @export
 rinflmixN <- function(N, l, p) {
-  # Use a transformation similar to one from Boehnig and Kuhnert (2006)
+  # Use a transformation similar to one from Boehning and Kuhnert (2006)
   q <- sapply(1:length(p), function(j) {
     p[j] / (1 - dpois(0, l[j])) / (sum(p / (1 - dpois(0, l))) + (1 - sum(p)))
   })
@@ -21,6 +21,6 @@ rinflmixN <- function(N, l, p) {
   y <- rpois(N, lmixed)
   y <- y[y != 0]
   change1s <- runif(length(y), 0, 1)
-  y[change1s <= (1 - sum(q))] <- 1
+  y[change1s <= (1 - sum(p))] <- 1
   return(y)
 }
